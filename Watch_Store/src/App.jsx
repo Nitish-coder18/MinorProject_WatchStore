70e7b12465190d3c656dd4e45a382d8300dd8680
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-09-04 22:13:03 +0530

    creating the navbaar and manage the data

--- Watch_Store/src/App.jsx
+++ Watch_Store/src/App.jsx
@@ -1,32 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbaar/Navbar"
-import Shop from "./pages/Shop"
-import ShopCategory from "./pages/ShopCategory"
-import Product from "./pages/Product"
-import LoginSingup from "./pages/LoginSignup"
-import Cart from "./pages/Cart"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <BrowserRouter>
+      <div> 
         <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="mens" />} />
-          <Route path="/womens" element={<ShopCategory category="womens"  />} />
-          <Route path="/kids" element={<ShopCategory category="kids"  />} />
-          <Route path="/product" element={<Product />}/>
-          <Route path=":productId" element={<Product />}/>
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSingup/>} />
-        </Routes>
-        </BrowserRouter>
-        
       </div>
     </>
   )
