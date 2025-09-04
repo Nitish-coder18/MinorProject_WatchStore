da31ceb543d3ab8af80418ff0bcb1cc5628ecdb2
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-09-04 18:24:26 +0530

    updation in projects

---
+++ Watch_Store/src/Assets/all_product.js
@@ -0,0 +1,403 @@
+import w_kids1 from "./w_kids1.jpeg";
+import w_kids2 from "./w_kids2.jpeg";
+import w_kids3 from "./w_kids3.jpeg";
+import w_kids4 from "./w_kids4.jpeg";
+import w_kids5 from "./w_kids5.jpeg";
+import w_kids6 from "./w_kids6.jpeg";
+import w_kids7 from "./w_kids7.jpeg";
+import w_kids8 from "./w_kids8.jpeg";
+import w_kids9 from "./w_kids9.jpeg";
+import w_kids10 from "./w_kids10.jpeg";
+import w_kids11 from "./w_kids11.jpeg";
+import w_kids12 from "./w_kids12.jpeg";
+
+import w_men1 from "./w_men1.jpeg";
+import w_men2 from "./w_men2.jpeg";
+import w_men3 from "./w_men3.jpeg";
+import w_men4 from "./w_men4.jpeg";
+import w_men5 from "./w_men5.jpeg";
+import w_men6 from "./w_men6.jpeg";
+import w_men7 from "./w_men7.jpeg";
+import w_men8 from "./w_men8.jpeg";
+import w_men9 from "./w_men9.jpeg";
+import w_men10 from "./w_men10.jpeg";
+import w_men11 from "./w_men11.jpeg";
+import w_men12 from "./w_men12.jpeg";
+
+import w_women1 from "./w_women1.jpeg";
+import w_women2 from "./w_women2.jpeg";
+import w_women3 from "./w_women3.jpeg";
+import w_women4 from "./w_women4.jpeg";
+import w_women5 from "./w_women5.jpeg";
+import w_women6 from "./w_women6.jpeg";
+import w_women7 from "./w_women7.jpeg";
+import w_women8 from "./w_women8.jpeg";
+import w_women9 from "./w_women9.jpeg";
+import w_women10 from "./w_women10.jpeg";
+import w_women11 from "./w_women11.jpeg";
+import w_women12 from "./w_women12.jpeg";
+
+
+
+let all_product = [
+    {
+        id: 1,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids1,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 2,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids2,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 3,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids3,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 4,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids4,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 5,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids5,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 6,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids6,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 7,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids7,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 8,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids8,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 9,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids9,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 10,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids10,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 11,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids11,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 12,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_kids12,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 13,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men1,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 14,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men2,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 15,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men3,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 16,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men4,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 17,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men5,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 18,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men6,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 19,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men7,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 20,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men8,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 21,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men9,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 22,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men10,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 23,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men11,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+{
+        id: 24,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_men12,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women1,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women2,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women3,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women4,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women5,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women6,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women7,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women8,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women9,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women10,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women11,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 25,
+        name: "lorem gipsum ke 8 sabd likh rhaa hooon phir edit krna baad me ",
+        category: "kids",
+        image: w_women12,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+
+
+]
+
+
+
+/* isko baad me edit krenge 
+
+import watch1 from "./watch1.png";
+import watch2 from "./watch2.png";
+import watch3 from "./watch3.png";
+import watch4 from "./watch4.png";
+import watch5 from "./watch5.png";
+import watch6 from "./watch6.png";
+import watch7 from "./watch7.png";
+import watch8 from "./watch8.png";
+import watch9 from "./watch9.png";
+import watch10 from "./watch10.png";
+import watch11 from "./watch11.png";
+import watch12 from "./watch12.png";
+import watch13 from "./watch13.png";
+import watch14 from "./watch14.png";
+import watch15 from "./watch15.png";
+import watch16 from "./watch16.png";
+import watch17 from "./watch17.png";
+import watch18 from "./watch18.png";
+import watch19 from "./watch19.png";
+import watch20 from "./watch20.png";
+import watch21 from "./watch21.png";
+import watch22 from "./watch22.png";
+import watch23 from "./watch23.png";
+import watch24 from "./watch24.png";
+import watch25 from "./watch25.png";
+import watch26 from "./watch26.png";
+import watch27 from "./watch27.png";
+import watch28 from "./watch28.png";
+import watch29 from "./watch29.png";
+import watch30 from "./watch30.png";
+
+const all_watches = [
+  { id: 1, name: "Rolex Submariner", brand: "Rolex", category: "man", price: 8500, image: watch1 },
+  { id: 2, name: "Omega Speedmaster", brand: "Omega", category: "man", price: 7200, image: watch2 },
+  { id: 3, name: "Casio G-Shock", brand: "Casio", category: "man", price: 150, image: watch3 },
+  { id: 4, name: "Fossil Gen 5", brand: "Fossil", category: "woman", price: 295, image: watch4 },
+  { id: 5, name: "Seiko 5 Automatic", brand: "Seiko", category: "man", price: 350, image: watch5 },
+  { id: 6, name: "Apple Watch Series 9", brand: "Apple", category: "woman", price: 499, image: watch6 },
+  { id: 7, name: "Timex Kids Analog", brand: "Timex", category: "kid", price: 90, image: watch7 },
+  { id: 8, name: "Swatch Originals", brand: "Swatch", category: "kid", price: 80, image: watch8 },
+  { id: 9, name: "Tag Heuer Carrera", brand: "Tag Heuer", category: "man", price: 4200, image: watch9 },
+  { id: 10, name: "Citizen Eco-Drive", brand: "Citizen", category: "man", price: 500, image: watch10 },
+  { id: 11, name: "Michael Kors Parker", brand: "Michael Kors", category: "woman", price: 250, image: watch11 },
+  { id: 12, name: "Daniel Wellington Classic", brand: "DW", category: "woman", price: 200, image: watch12 },
+  { id: 13, name: "Garmin Forerunner 55", brand: "Garmin", category: "man", price: 180, image: watch13 },
+  { id: 14, name: "Samsung Galaxy Watch 6", brand: "Samsung", category: "woman", price: 399, image: watch14 },
+  { id: 15, name: "Casio Baby-G", brand: "Casio", category: "kid", price: 120, image: watch15 },
+  { id: 16, name: "Rolex Datejust", brand: "Rolex", category: "woman", price: 9500, image: watch16 },
+  { id: 17, name: "Fossil Townsman", brand: "Fossil", category: "man", price: 220, image: watch17 },
+  { id: 18, name: "Tissot PRX", brand: "Tissot", category: "man", price: 650, image: watch18 },
+  { id: 19, name: "Swatch Flik Flak", brand: "Swatch", category: "kid", price: 60, image: watch19 },
+  { id: 20, name: "Casio Vintage", brand: "Casio", category: "woman", price: 75, image: watch20 },
+  { id: 21, name: "Hamilton Khaki Field", brand: "Hamilton", category: "man", price: 995, image: watch21 },
+  { id: 22, name: "Movado Bold", brand: "Movado", category: "woman", price: 350, image: watch22 },
+  { id: 23, name: "Cartier Tank", brand: "Cartier", category: "woman", price: 12000, image: watch23 },
+  { id: 24, name: "Rado Centrix", brand: "Rado", category: "man", price: 1100, image: watch24 },
+  { id: 25, name: "Tommy Hilfiger Analog", brand: "Tommy Hilfiger", category: "man", price: 180, image: watch25 },
+  { id: 26, name: "Guess Rose Gold", brand: "Guess", category: "woman", price: 270, image: watch26 },
+  { id: 27, name: "Diesel Mr. Daddy", brand: "Diesel", category: "man", price: 400, image: watch27 },
+  { id: 28, name: "Casio Youth Series", brand: "Casio", category: "kid", price: 65, image: watch28 },
+  { id: 29, name: "Longines HydroConquest", brand: "Longines", category: "man", price: 1300, image: watch29 },
+  { id: 30, name: "Michael Kors Slim Runway", brand: "Michael Kors", category: "woman", price: 210, image: watch30 },
+];
+
+export default all_watches; */
