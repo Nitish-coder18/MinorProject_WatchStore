e95a06df26c214e7b04df4995e1a6759f9a9e181
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-09-05 23:15:23 +0530

    update the footer

--- Watch_Store/src/pages/Shop.jsx
+++ Watch_Store/src/pages/Shop.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero/Hero'
 import { Popular } from '../components/Popular/Popular'
 import { Offer } from '../components/Offers/Offer'
 import { NewCollection } from '../components/NewCollections/NewCollection'
+import { NewsLetter } from '../components/NewsLetter/NewsLetter'
+
 
 const Shop = () => {
   return (
@@ -11,6 +13,8 @@ const Shop = () => {
       <Popular />
       <Offer />
       <NewCollection />
+      <NewsLetter />
+      
     </div>
   )
 }
