4a39bd3418972cc01e5578de893421c2545f4295
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-09-05 18:06:00 +0530

    hero section updated

--- Watch_Store/src/components/Hero/Hero.jsx
+++ Watch_Store/src/components/Hero/Hero.jsx
@@ -6,30 +6,32 @@ import hero_image from '../Assets/hero_image.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className="hero">
+      <div className="hero-left">
 
-        <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
+        <h2>NEW ARRIVALS ONLY</h2>
 
-            <div className="hero-hand-icon">
-                <p>new</p>
-                <img src={hand_icon} alt="" />
-            </div>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
 
-            <p>collections</p>
-            <p>for everyone</p>
+            <img src={hand_icon} alt="" />
+          </div>
 
-        </div>
+          <p>collections</p>
 
-        <div className="hero-latest-button">
-            <div>Latest Collection</div>
-            <img src={Arrow_icon} alt="" />
-        </div>
-
-        <div className="hero-right">
-            <img src={hero_image} alt="" />
+          <p>for everyone</p>
 
         </div>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={Arrow_icon} alt="" width="35px" height="35px" color='white' />
+        </div>
+      </div>
+
+      <div className="hero-right"> 
+        <img src={hero_image} alt="" width="700px" height="250px" />
+      </div>
       
     </div>
   )
