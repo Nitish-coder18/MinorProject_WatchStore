6444dbe7a1edd25008f0762d4993ee106352939d
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-09-05 22:45:35 +0530

    Add new watch products to New_Collections data

--- Watch_Store/src/components/Assets/New_Collections.js
+++ Watch_Store/src/components/Assets/New_Collections.js
@@ -33,6 +33,34 @@ let data_product = [
         new_price:54.00,
         old_price:123.50,
      },
+     {
+        id: 5,
+        name: "Rolex Submariner",  
+        image: p1_img,
+        new_price:50.00,
+        old_price:120.50,
+     },
+     {
+        id: 6,
+        name: "Omega Speedmaster",  
+        image: p2_img,
+        new_price:45.00,
+        old_price:125.50,
+     },
+     {
+        id: 7,
+        name: "Casio G-Shock",  
+        image: p3_img,
+        new_price:97.00,
+        old_price:127.50,
+     },
+     {
+        id: 8,
+        name: "Casio",  
+        image: p4_img,
+        new_price:54.00,
+        old_price:123.50,
+     },
   ]
 
   export default data_product;
