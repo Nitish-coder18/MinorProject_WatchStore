05263419a75023f23bdc36be83d15ef8e0509c5f
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-09-05 22:29:49 +0530

    updation of new collections

---
+++ Watch_Store/src/components/Assets/New_Collections.js
@@ -0,0 +1,38 @@
+import p1_img from "./w_men1.jpg";
+import p2_img from "./w_kids12.jpeg";
+import p3_img from "./w_men3.jpeg";
+import p4_img from "./w_women4.png";
+
+
+let data_product = [   
+    {
+        id: 1,
+        name: "Rolex Submariner",  
+        image: p1_img,
+        new_price:50.00,
+        old_price:120.50,
+     },
+     {
+        id: 2,
+        name: "Omega Speedmaster",  
+        image: p2_img,
+        new_price:45.00,
+        old_price:125.50,
+     },
+     {
+        id: 3,
+        name: "Casio G-Shock",  
+        image: p3_img,
+        new_price:97.00,
+        old_price:127.50,
+     },
+     {
+        id: 4,
+        name: "Casio",  
+        image: p4_img,
+        new_price:54.00,
+        old_price:123.50,
+     },
+  ]
+
+  export default data_product;
