070ff4d90eebec64b6e014e8b3c720557a540071
sakshishr <sakshisharma22092006@gmail.com>
2025-09-06 12:26:58 +0530

    shopcategory made

--- Watch_Store/src/pages/ShopCategory.jsx
+++ Watch_Store/src/pages/ShopCategory.jsx
@@ -7,10 +7,10 @@ const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   return (
     <div className='shop-category'>
-      <img src={props.banner}/>
-      <div className='shopcategory-indexshort'>
+      <img  className="shopcategory-banner" src={props.banner}/>
+      <div className='shopcategory-indexsort'>
         <p>
-          <span>Showing 1-12 </span> out of 30 products
+          <span>Showing 1-12 </span> out of 36 products
         </p>
         <div className='shopcategory-sort'>
            Sort by <img src={dropdown_icon}/>
